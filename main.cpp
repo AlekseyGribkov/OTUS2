62cc750c40d4ed37faf62f333a300a36939d75e6
AlekseyGribkov <Aleksey.V.Gribkov@rt.ru>
2020-07-06 17:19:13 +0300

    delete test

--- main.cpp
+++ main.cpp
@@ -6,41 +6,16 @@
 
 #include <utility>
 #include <map>
-#include <limits>
 
 std::multimap<int, std::string, std::greater<int>> sorted_map;
 std::vector<std::string> vSrt;
-std::vector<std::string> mySplit(std::string &str, char delimiter = '\t');
-//std::pair<std::string, std::string>  mySplitStr(std::string &str, char delimiter = '\t');
 std::pair<std::string, std::string>  mySplitStr(std::string &str, char delimiter = '\t');
 void mySort(std::string str,
 std::multimap<std::string, std::string, std::greater<std::string>> &res);
 std::vector<std::string> Where(std::multimap<std::string, std::string, std::greater<std::string>> multiMap, std::string predicate, bool isAll = false);
 void Print(std::multimap<std::string, std::string, std::greater<std::string>> multiMap);
 void Print(std::vector<std::string> myVec);
-
 std::vector<std::string> split(const std::string &s, char delim);
-
-void StartApp();
-
-void test()
-{
-    std::multimap<int, std::string, std::greater<int>> sorted_map;
-    // Sort the names of animals in descending order of the number of legs
-    sorted_map.insert(std::make_pair(6,   "bug"));
-    sorted_map.insert(std::make_pair(4,   "cat"));
-    sorted_map.insert(std::make_pair(100, "centipede"));
-    sorted_map.insert(std::make_pair(2,   "chicken"));
-    sorted_map.insert(std::make_pair(0,   "fish"));
-    sorted_map.insert(std::make_pair(4,   "horse"));
-    sorted_map.insert(std::make_pair(8,   "spider"));
-
-    for (auto const& entry: sorted_map)
-    {
-        std::cout << entry.second << " (has " << entry.first << " legs)" << '\n';
-    }
-}
-
 
 
 int main(int argc, char const *argv[])
@@ -49,16 +24,19 @@ int main(int argc, char const *argv[])
     std::multimap<std::string, std::string, std::greater<std::string>> multiMap;
     for(std::string line; std::getline(std::cin, line);)
     {
+        if(line == "") return 0;
         auto res = mySplitStr(line);
-        
-        multiMap.emplace(res.first, res.first);
-        Print(multiMap);
-        auto vres = Where(multiMap, "1.");
-        Print(vres);
-        auto vres1 = Where(multiMap, "46.70.");
-        Print(vres1);
-        auto vres2 = Where(multiMap, "46", true);
-        Print(vres2);
+        if(res.first != "")
+        {            
+            multiMap.emplace(res.first, res.first);
+            Print(multiMap);
+            auto vres = Where(multiMap, "1.");
+            Print(vres);
+            auto vres1 = Where(multiMap, "46.70.");
+            Print(vres1);
+            auto vres2 = Where(multiMap, "46", true);
+            Print(vres2);
+        }
         
     }
 
@@ -93,32 +71,6 @@ void Print(std::multimap<std::string, std::string, std::greater<std::string>> mu
     }
 }
 
-/***************** StartApp ***********/
-void StartApp()
-{
-  std::multimap<std::string, std::string, std::greater<std::string>> multiMap;
-
-    std::string _str = "1\twold\t!";
-    auto res = mySplitStr(_str);
-    multiMap.emplace(res.first, res.second);
-
-     std::string str2 = "2\twold2\t!";
-    auto res2 = mySplitStr(str2);
-    multiMap.emplace(res2.first, res2.second);
-
-    for (auto const& entry: multiMap)
-    {
-        std::cout <<  entry.second << '\n';
-    }
-
-     auto vres = Where(multiMap, "1");
-
-    for(auto const& value: vres)
-    {
-       std::cout << value << '\n';
-    }
-}
-#include <limits>
 
 /***************** Where ***************/
 std::vector<std::string> Where(std::multimap<std::string, std::string, std::greater<std::string>> multiMap, std::string predicate, bool isAll)
@@ -172,23 +124,3 @@ std::pair<std::string, std::string> mySplitStr(std::string &str, char delimiter)
   return {"", ""};
 }
 
-
-/******************  mySplit ****************/
-std::vector<std::string> mySplit(std::string &str, char delimiter)
-{
-   std::vector<std::string> res;
-   try
-   {
-       std::stringstream ss(str);
-       std::string tmp;
-       while (std::getline(ss, tmp, delimiter)) {
-         res.push_back(tmp);
-         }
-   }
-   catch(const std::exception& e)
-   {
-       std::cerr << e.what() << '\n';
-   }
-
-   return res;
-}
